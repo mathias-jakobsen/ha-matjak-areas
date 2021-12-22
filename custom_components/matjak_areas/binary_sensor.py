c95f22a6e70a87fac1320dad1fad7dea1bedb524
Mathias Jakobsen <mathias@jakobsen.in>
2021-12-22 21:05:16 +0000

    main

--- custom_components/matjak_areas/binary_sensor.py
+++ custom_components/matjak_areas/binary_sensor.py
@@ -15,6 +15,7 @@ from .const import (
 from .utils.ma_entity import MA_BinarySensorEntity
 from .utils.ma_registry import MA_Registry
 from homeassistant.components.binary_sensor import DOMAIN as BINARY_SENSOR_DOMAIN, BinarySensorDeviceClass
+from homeassistant.components.media_player import DOMAIN as MEDIA_PLAYER_DOMAIN
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_ENTITY_ID, STATE_ON, STATE_UNAVAILABLE
 from homeassistant.core import HomeAssistant
@@ -87,14 +88,14 @@ class PresenceSensor(MA_BinarySensorEntity):
     #--------------------------------------------#
 
     def __init__(self, registry: MA_Registry, config: PresenceConfig):
-        self._config         : PresenceConfig = config
-        self._device_classes : list[str]      = config.get(CONF_BINARY_SENSOR_DEVICE_CLASSES) + config.get(CONF_MEDIA_PLAYER_DEVICE_CLASSES)
-        self._domains        : list[str]      = config.get(CONF_DOMAINS)
-        self._entities       : list[str]      = []
-        self._entities_on    : list[str]      = []
-        self._registry       : MA_Registry    = registry
-        self._states_on      : list[str]      = config.get(CONF_STATES_ON)
-        self._state_listener : Callable       = None
+        self._config         : PresenceConfig       = config
+        self._device_classes : dict[str, list[str]] = { BINARY_SENSOR_DOMAIN: config.get(CONF_BINARY_SENSOR_DEVICE_CLASSES), MEDIA_PLAYER_DOMAIN: config.get(CONF_MEDIA_PLAYER_DEVICE_CLASSES) }
+        self._domains        : list[str]            = config.get(CONF_DOMAINS)
+        self._entities       : list[str]            = []
+        self._entities_on    : list[str]            = []
+        self._registry       : MA_Registry          = registry
+        self._states_on      : list[str]            = config.get(CONF_STATES_ON)
+        self._state_listener : Callable             = None
 
 
     #--------------------------------------------#
@@ -127,7 +128,7 @@ class PresenceSensor(MA_BinarySensorEntity):
     async def async_setup(self, *args: Any) -> None:
         """ Triggered when the entity is being setup. """
         self.async_on_remove(self._registry.add_update_listener(self.async_on_registry_updated))
-        self._entities = self._registry.get_entities(domains=self._domains, device_classes=self._device_classes)
+        self._entities = self._get_entities()
         self._setup_listeners()
 
     async def async_update_state(self) -> None:
@@ -148,7 +149,7 @@ class PresenceSensor(MA_BinarySensorEntity):
 
     async def async_on_registry_updated(self) -> None:
         """ Triggered when the MA_Registry is updated. """
-        self._entities = self._registry.get_entities(domains=self._domains, device_classes=self._device_classes)
+        self._entities = self._get_entities()
         self._setup_listeners()
         await self.async_update_state()
 
@@ -160,6 +161,14 @@ class PresenceSensor(MA_BinarySensorEntity):
     #--------------------------------------------#
     #       Private Methods
     #--------------------------------------------#
+
+    def _get_entities(self) -> list[str]:
+        result = []
+
+        for domain in self._domains:
+            result = result + self._registry.get_entities(domains=[domain], device_classes=self._device_classes.get(domain, []))
+
+        return result
 
     def _get_entities_on(self) -> list[str]:
         """ Gets the entities that are on. """
